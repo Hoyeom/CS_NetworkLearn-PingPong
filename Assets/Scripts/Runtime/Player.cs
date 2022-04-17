2caab86b502be522f161d81f2a659cfd4f848109
Hoyeom <korehdrb@gmail.com>
2022-04-17 17:51:24 +0900

    PingPong Complete

--- Assets/Scripts/Runtime/Player.cs
+++ Assets/Scripts/Runtime/Player.cs
@@ -8,36 +8,35 @@ namespace DefaultNamespace
 {
     public class Player : NetworkBehaviour
     {
-        [SerializeField] private float speed = 4; 
+        [SerializeField] private float speed = 4;
         private Vector2 inputVector = Vector2.zero;
 
-        public override void OnStartServer()
-        {
-            this.FixedUpdateAsObservable()
-                .Select(_ => inputVector)
-                .Where(x=>x.sqrMagnitude>0)
-                .Subscribe(_ => Move());
-        }
-
+        public override void OnStartServer() => ServerMoveStream();
         private void Start()
         {
             if (!isLocalPlayer) { return; }
+            InputStream();
+        }
 
-            this.UpdateAsObservable()
-                .Select(_ => new Vector2(0, Input.GetAxisRaw("Vertical")))
-                .Subscribe(CmdInputVec);
-        }
-        
+        #region Stream
+
+        private void ServerMoveStream() => this.FixedUpdateAsObservable()
+            .Select(_ => inputVector)
+            .Where(x => x.sqrMagnitude > 0)
+            .Subscribe(Move);
+
+        private void InputStream() => this.UpdateAsObservable()
+            .Select(_ => new Vector2(0, Mathf.Min(Input.GetAxisRaw("Vertical"),1)))
+            .Subscribe(ClientInput);
+
+        #endregion
+
+        [Client]
+        private void ClientInput(Vector2 value) => CmdInputVector(value);
         [Command]
-        private void CmdInputVec(Vector2 input)
-        {
-            inputVector = input;
-        }
-        
-        private void Move()
-        {
-            transform.Translate(inputVector * speed * Time.fixedDeltaTime);
-            Debug.Log($"뭐냐{inputVector}");
-        }
+        private void CmdInputVector(Vector2 value) => SetInputVector(value);
+        [Server]
+        private void SetInputVector(Vector2 value) => inputVector = value;
+        private void Move(Vector2 value) => transform.Translate(value * speed * Time.fixedDeltaTime);
     }
 }
